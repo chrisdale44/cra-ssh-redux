13db3eaed328c6a87475bfcc5d6e1591bca28bb5
christopherdale <christopher.j.dale@baesystems.com>
2019-04-17 02:26:43 +0100

    work experience styling

--- src/apps/portfolio/components/CodeSamples/index.js
+++ src/apps/portfolio/components/CodeSamples/index.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
+import Slider from "react-slick";
 import styles from "./Projects.module.css";
 
 class CodeSamples extends Component {
   render() {
     const { samples } = this.props;
+    const settings = {
+      dots: true,
+      slidesToShow: 2,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 0
+          }
+        }
+      ]
+    };
     return (
       <section>
-        <h2>Example Code</h2>
-        {samples.map(({ title, technologies }, i) => (
-          <div key={i}>
-            <h3>{title}</h3>
-            <span>{technologies && technologies.map(t => t + ", ")}</span>
-          </div>
-        ))}
+        <h2>Code Samples</h2>
+        <div className={styles.sliderContainer}>
+          <Slider {...settings}>
+            {samples.map(({ title, technologies }, i) => (
+              <div>
+                <div key={i} className={styles.sampleTile}>
+                  <h3>{title}</h3>
+                  <span>{technologies && technologies.map(t => t + ", ")}</span>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </section>
     );
   }
--- src/apps/portfolio/components/WorkExperience/index.js
+++ src/apps/portfolio/components/WorkExperience/index.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import classNames from "classnames/bind";
 import { dateRange } from "./helpers";
 import componentStyles from "./WorkExperience.module.css";
 import icons from "../../icons/icons.module.css";
 const styles = { ...componentStyles, ...icons };
 
+let cx = classNames.bind(styles);
+
 class WorkExperience extends Component {
   render() {
     const { experience } = this.props;
     const settings = {
-      dots: true
+      dots: true,
+      slidesToShow: 2,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 0
+          }
+        }
+      ]
     };
     return (
       <section>
@@ -27,72 +42,104 @@ class WorkExperience extends Component {
               technologies
             },
             i
-          ) => (
-            <article className={styles.jobContainer} key={i}>
-              {company && role && (
-                <>
-                  {company.logo && <span className={styles[company.logo]} />}
-                  <div className={styles.companyDetails}>
-                    <h3>
-                      {company.name}: {role}
-                    </h3>
-                    <span className={styles.dateRange}>
-                      {dateRange(startDate, endDate)}
-                    </span>
-                  </div>
-                </>
-              )}
-
-              {company && <p>{company.description}</p>}
-              <p>{description}</p>
-              {responsibilities && (
-                <ul>
-                  {responsibilities.map(responsibility => (
-                    <li>{responsibility}</li>
-                  ))}
-                </ul>
-              )}
-              <span className={styles.technologies}>
-                {technologies && technologies.map(t => t + ", ")}
-              </span>
-              {projects && <h3>Projects</h3>}
-              <div class={styles.sliderContainer}>
-                <Slider {...settings}>
-                  {projects &&
-                    projects.map(
-                      ({
-                        title,
-                        startDate,
-                        endDate,
-                        description,
-                        responsibilities,
-                        technologies,
-                        logo
-                      }) => (
-                        <div className={styles.slideContainer}>
-                          <div className={styles.slide}>
-                            {logo && <span className={styles[logo]} />}
-                            <h4>{title}</h4>
-                            {dateRange(startDate, endDate)}
-                            <p>{description}</p>
-                            {responsibilities && (
-                              <ul>
-                                {responsibilities.map(responsibility => (
-                                  <li>{responsibility}</li>
-                                ))}
-                              </ul>
-                            )}
-                            <span>
-                              {technologies && technologies.map(t => t + ", ")}
-                            </span>
-                          </div>
-                        </div>
-                      )
+          ) => {
+            settings.infinite = projects && projects.length > 3;
+            settings.slidesToShow = projects && projects.length > 1 ? 2 : 1;
+            return (
+              <article className={styles.jobContainer} key={i}>
+                {company && role && (
+                  <div className={styles.heading}>
+                    {company.logo && (
+                      <span className={cx(styles[company.logo], styles.icon)} />
                     )}
-                </Slider>
-              </div>
-            </article>
-          )
+                    <div className={styles.mainDetails}>
+                      <h3>
+                        {company.name}: {role}
+                      </h3>
+                      <span className={styles.dateRange}>
+                        {dateRange(startDate, endDate)}
+                      </span>
+                      <br />
+                      {technologies && (
+                        <span className={styles.techList}>
+                          {technologies.map((t, i) =>
+                            i < technologies.length ? t + ", " : t
+                          )}
+                        </span>
+                      )}
+                    </div>
+                  </div>
+                )}
+
+                {company && <p>{company.description}</p>}
+                <p>{description}</p>
+                {responsibilities && (
+                  <ul>
+                    {responsibilities.map(responsibility => (
+                      <li>{responsibility}</li>
+                    ))}
+                  </ul>
+                )}
+
+                {projects && <h3>Projects</h3>}
+                <div class={styles.sliderContainer}>
+                  <Slider {...settings}>
+                    {projects &&
+                      projects.map(
+                        ({
+                          title,
+                          startDate,
+                          endDate,
+                          description,
+                          responsibilities,
+                          technologies,
+                          logo
+                        }) => (
+                          <div className={styles.slideContainer}>
+                            <div className={styles.slide}>
+                              <div className={styles.heading}>
+                                {logo && (
+                                  <span
+                                    className={cx(styles[logo], styles.icon)}
+                                  />
+                                )}
+                                <div className={styles.mainDetails}>
+                                  <h4>{title}</h4>
+                                  {startDate && endDate && (
+                                    <span className={styles.dateRange}>
+                                      {dateRange(startDate, endDate)}
+                                      <br />
+                                    </span>
+                                  )}
+
+                                  {technologies && (
+                                    <span className={styles.techList}>
+                                      {technologies.map((t, i) =>
+                                        i < technologies.length - 1
+                                          ? t + ", "
+                                          : t
+                                      )}
+                                    </span>
+                                  )}
+                                </div>
+                              </div>
+                              <p>{description}</p>
+                              {responsibilities && (
+                                <ul>
+                                  {responsibilities.map(responsibility => (
+                                    <li>{responsibility}</li>
+                                  ))}
+                                </ul>
+                              )}
+                            </div>
+                          </div>
+                        )
+                      )}
+                  </Slider>
+                </div>
+              </article>
+            );
+          }
         )}
       </section>
     );
